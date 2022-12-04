e9727183b096f7608f3d30588d39eaedd76ee8e0
Azyah Martin <72221361+AMartin4@users.noreply.github.com>
2022-12-03 21:54:50 -0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,5 +1,33 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lab1.h"
+
+
+char* readString(char* file){
+
+    char* ptr = malloc(MAX_LINE_LEN*sizeof(char));
+    FILE* txt = fopen(file,"r");
+    strcpy(ptr,fgets(ptr,MAX_LINE_LEN-1, txt));
+    fclose(txt);
+
+    return ptr;
+
+}
+
+char* mysteryExplode(const char* str){
+
+    int len = strlen(str)-1;
+    char* newStr = calloc(((len*(len+1))/2));
+    int count = 0;
+    for(int j = 0; j < len; j++){
+
+        for(int k = 0; k < i+1; k++){
+
+            strncat(newStr,&str[k],1);
+        }
+    }
+
+    return newStr;
+}
 
